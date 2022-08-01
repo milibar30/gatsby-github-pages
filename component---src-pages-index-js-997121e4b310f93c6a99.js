aa4ab728863160f2df85503c371902403de93ed3
milibar <milind.barman30@gmail.com>
2022-08-01 15:29:18 +0530

    Updates

---
+++ component---src-pages-index-js-997121e4b310f93c6a99.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkismir_copy=self.webpackChunkismir_copy||[]).push([[678],{6558:function(e,t,s){s.r(t);var i=s(7294);t.default=function(){return i.createElement("div",{className:"text-3xl font-bold underline"},"This is a test")}}}]);
+//# sourceMappingURL=component---src-pages-index-js-997121e4b310f93c6a99.js.map
