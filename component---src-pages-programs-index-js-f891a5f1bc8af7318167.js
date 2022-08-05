2f87796b0274320487e5a5fd54a49482f33db514
milibar <milind.barman30@gmail.com>
2022-08-05 17:43:21 +0530

    Updates

---
+++ component---src-pages-programs-index-js-f891a5f1bc8af7318167.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkismir_copy=self.webpackChunkismir_copy||[]).push([[380],{9567:function(e,c,l){l.r(c);var s=l(7294),a=l(4185);c.default=function(){return s.createElement("div",{className:"bg-[#E9ECEF] h-screen"},s.createElement(a.Z,null),s.createElement("div",{className:"flex flex-col space-y-5 mx-20 "}))}}}]);
+//# sourceMappingURL=component---src-pages-programs-index-js-f891a5f1bc8af7318167.js.map
